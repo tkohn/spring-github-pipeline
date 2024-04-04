30c89a81b01dbabeb0b1f67a0fdcab414fb389c3
Torsten Kohn <tkohn@users.noreply.github.com>
2024-04-04 20:20:18 +0200

    init project via start.spring.io

---
+++ src/main/java/dev/kohn/example/pipeline/springgithubpipeline/SpringGithubPipelineApplication.java
@@ -0,0 +1,13 @@
+package dev.kohn.example.pipeline.springgithubpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubPipelineApplication.class, args);
+	}
+
+}
